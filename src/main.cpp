5ef7469ce18abdb53bbb2398befb51c921f12646
Martijn van Iersel <mvaniersel@gmail.com>
2026-01-26 22:38:59 +0100

    Fix USE_MOUSE

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@ int main(int argc, const char *const *argv)
 		.setTitle("P.E.N.G.U.I.N!")
 		.setAppName("PENGUIN")
 		.setResizableWindow(true)
-		.setPreferredDisplaySize(1024, 768);
+		.setPreferredDisplaySize(1024, 768)
+		.setMouseEnabled(true);
 
 	if (!mainloop.init(argc, argv)) {
 		auto engine = make_shared<Engine>();
