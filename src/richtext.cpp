be0e2cd0e3f9aa7c17fa0fcd28d10b9697c19f41
Martijn van Iersel <mvaniersel@gmail.com>
2026-02-17 09:11:21 +0100

    Switch from dom parser to streaming parser. Separate style attributes from implementation

--- src/richtext.cpp
+++ src/richtext.cpp
@@ -2,7 +2,7 @@
 
 #include <string>
 #include <list>
-#include "dom.h"
+#include "xml.h"
 #include "strutil.h"
 #include <allegro5/allegro_color.h>
 #include <allegro5/allegro_font.h>
@@ -12,56 +12,18 @@
 
 using namespace std;
 
+/**
+ * Linearized rich text.
+ */
 struct TextSpan {
-	enum Type {
-		TYPE_PLAIN,
-		TYPE_BOLD,
-		TYPE_ITALIC,
-		TYPE_HEADER,
-		TYPE_LINK,
-		TYPE_LINEBREAK
-	};
+	string tag; // NOTE: if we want to store bold AND italic, we'd have to expand this to a set of tags.
+	string content;
+	map<string, string> attributes; // used for e.g. href.
 
-	Type type;
-	std::string content;
-	std::string href;  // only for TYPE_LINK
-
-	TextSpan(Type t, const std::string &c) : type(t), content(c) {}
+	TextSpan(): tag(), content(), attributes() {}
+	TextSpan(const string &_tag, const string &c) : tag(_tag), content(c), attributes() {}
+	TextSpan(const string &_tag, const string &c, const map<string, string> &attr) : tag(_tag), content(c), attributes(attr) {}
 };
-
-
-void visit(const xdom::DomNode &root, std::list<TextSpan> &spans) {
-	// process cdata and children in parallel
-	for (int i = 0; i < (int)root.cdata.size(); ++i) {
-		std::string cdata = root.cdata[i];
-		if (cdata != "") {
-			spans.push_back(TextSpan(TextSpan::TYPE_PLAIN, cdata));
-		}
-		if (i < (int)root.children.size()) {
-			const xdom::DomNode &child = root.children[i];
-			if (child.name == "b") {
-				std::string cdata = join(child.cdata, "");
-				spans.push_back(TextSpan(TextSpan::TYPE_BOLD, cdata));
-			} else if (child.name == "i") {
-				std::string cdata = join(child.cdata, "");
-				spans.push_back(TextSpan(TextSpan::TYPE_ITALIC, cdata));
-			} else if (child.name == "h1") {
-				std::string cdata = join(child.cdata, "");
-				spans.push_back(TextSpan(TextSpan::TYPE_HEADER, cdata));
-			} else if (child.name == "a") {
-				std::string cdata = join(child.cdata, "");
-				TextSpan linkSpan(TextSpan::TYPE_LINK, cdata);
-				linkSpan.href = child.attributes.at("href");
-				spans.push_back(linkSpan);
-			} else if (child.name == "br") {
-				spans.push_back(TextSpan(TextSpan::TYPE_LINEBREAK, ""));
-			} else {
-				// NOTE we're not recursing for known tags.
-				visit(child, spans);
-			}
-		}
-	}
-}
 
 std::string replaceAll(std::string input, const std::string &replace_word, const std::string &replace_by) {
 	// Find the first occurrence of the substring
@@ -78,20 +40,6 @@ std::string replaceAll(std::string input, const std::string &replace_word, const
 						 pos + replace_by.size());
 	}
 	return input;
-}
-std::list<TextSpan> spansFromText(std::string const &text) {
-	// replace double newlines with <br> tags
-	std::string processed = "<root>" + replaceAll(text, "\n\n", "<br/>") + "</root>";
-	processed = replaceAll(processed, "\n", " "); //TODO: remove tabs and multiple spaces too?
-
-	xdom::DomParser parser = xdom::DomParser(processed);
-	xdom::DomNode root = parser.parse();
-
-	// now walk the tree emitting TextSpan objects
-	std::list<TextSpan> spans;
-	visit(root, spans);
-
-	return spans;
 }
 
 struct CallBackContext {
@@ -118,9 +66,9 @@ bool cb(int line_num, float xflow, float yflow, const ALLEGRO_USTR *line, void *
 
 	auto t = Text::build(s->color, 0, copy).font(s->font).xy(x, y).get();
 
-	if (s->span->type == TextSpan::TYPE_LINK) {
+	if (s->span->tag == "a") {
 		t->setDecoration(TextStyle::UNDERLINE);
-		string hrefcpy = s->span->href;
+		string hrefcpy = s->span->attributes["href"];
 		t->onClick([=](){ openLink(hrefcpy); });
 	}
 	// else {
@@ -134,11 +82,54 @@ bool cb(int line_num, float xflow, float yflow, const ALLEGRO_USTR *line, void *
 	return true;
 }
 
+/** 
+ * Linearizes html-like rich text string into a sequence of spans, each span having a single style. 
+ * throws XmlException if the string is invalid XML.
+ **/
+list<TextSpan> spansFromHtml(const string &str) {
+	list<TextSpan> textSpans;
+	TextSpan currentSpan { "", "" };
+	bool hasContent = false;
+	XmlParser parser(str);
+	while (parser.next() != XmlParser::END_DOCUMENT) {
+		switch (parser.getEventType()) {
+			case XmlParser::START_TAG:
+				if (hasContent) {
+					textSpans.push_back(currentSpan);
+				}
+				currentSpan = TextSpan(parser.getName(), "", parser.getAttributes());
+				hasContent = true;
+				break;
+			case XmlParser::END_TAG:
+				if (hasContent) {
+					textSpans.push_back(currentSpan);
+				}
+				currentSpan = TextSpan("", "");
+				hasContent = false;
+				break;
+			case XmlParser::TEXT: {
+				hasContent = true;
+				std::string text = parser.getText();
+				if (!text.empty()) {
+					// TODO: also flatten consecutive whitespace
+					currentSpan.content += text;
+				}
+				break;
+			}
+			default: break;
+		}
+	}
+	if (hasContent) {
+		textSpans.push_back(currentSpan);
+	}
+	return textSpans;
+}
+
 void appendRichText(
 	const char *s, float *xflow, float *yflow, int iw, list<ComponentPtr> &components, const StyleData &style
 ) {
 	list<TextSpan> spans;
-	spans = spansFromText(s);
+	spans = spansFromHtml(s);
 	// we parse html into spans
 	for(auto &span : spans) {
 
@@ -151,27 +142,25 @@ void appendRichText(
 		ctx.font = nullptr;
 		ctx.color = style.textColor;
 		ctx.line_height = al_get_font_line_height(style.normal);
-		switch(span.type) {
-			case TextSpan::TYPE_HEADER:
-				ctx.font = style.header;
-				ctx.line_height = al_get_font_line_height(style.header) * 1.5;
-				break;
-			case TextSpan::TYPE_BOLD:
-				ctx.font = style.bold;
-				break;
-			case TextSpan::TYPE_ITALIC:
-				ctx.font = style.italic;
-				break;
-			case TextSpan::TYPE_PLAIN:
-				ctx.font = style.normal;
-				break;
-			case TextSpan::TYPE_LINK:
-				ctx.font = style.normal;
-				ctx.color = style.linkColor;
-				break;
-			case TextSpan::TYPE_LINEBREAK:
-				// no styling associated with line breaks.
-				break;
+		if(span.tag == "h1") {
+			ctx.font = style.header;
+			ctx.line_height = al_get_font_line_height(style.header) * 1.5;
+		}
+		else if (span.tag == "b") {
+			ctx.font = style.bold;
+		}
+		else if (span.tag == "i") {
+			ctx.font = style.italic;
+		}
+		else if (span.tag == "") {
+			ctx.font = style.normal;
+		}
+		else if (span.tag == "a") {
+			ctx.font = style.normal;
+			ctx.color = style.linkColor;
+		}
+		else if (span.tag == "br") {
+			// no styling associated with line breaks.
 		}
 
 		// draw_multiline_text(font, color, 8, 8, &xcursor, &ycursor, iw, th, 0, span.content.c_str());
@@ -181,7 +170,7 @@ void appendRichText(
 		// span.linkHotspots = std::vector<Rect>();
 		do_multiline_ustr(ctx.font, xflow, yflow, ctx.line_height, iw, al_ref_cstr(&info, span.content.c_str()), cb, &ctx);
 
-		if (span.type == TextSpan::TYPE_LINEBREAK || span.type == TextSpan::TYPE_HEADER) {
+		if (span.tag == "br" || span.tag == "h1") {
 			*xflow = 0;
 			*yflow += ctx.line_height;
 		}
